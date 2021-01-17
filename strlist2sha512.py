34c60ab2021883a1dd5c2ec504e52a17a5ab9c17
hypostat1c <b0d3ns33@criminal.li>
2021-01-17 20:32:13 +0100

    Create strlist2sha512.py

---
+++ strlist2sha512.py
@@ -0,0 +1,18 @@
+import hashlib
+
+def main():
+    try:
+        with open("output.txt", "r") as file:
+            nice = file.read().splitlines()
+            file.close()
+            vide = []
+            for i in nice:
+                vide.append(i + ":" + hashlib.sha512(i.encode('utf-8')).hexdigest() + "\n")
+
+            listToStr = ' '.join([str(elem) for elem in vide]) 
+            final = open("salope.txt", "w")
+            final.write(listToStr)
+    except:
+        pass
+
+main()
